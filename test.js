b6007830f47f33ab6fc5cbc42dd52f5bc46c9cca
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-01-24 12:51:05 -0500

    update testing functionality

--- test.js
+++ test.js
@@ -11,6 +11,13 @@
 // general testing framework
 // takes in a function to test, an ordered list of its inputs, and the expected result
 var test_function = function(function_name, inputs, expected_result) {
-	return function_name(inputs) === expected_result
+	var result = function_name(inputs); // compute the result on the given input
+	console.log("---Testing function ",function_name," on input ", inputs,"---"); // intro output to console
+	console.log("\tResult: ", result, " -- Expected result: ", expected_result); // computation output to console
+	var success = (result === expected_result); // see if the result matches what's expected
+	if(success)
+		success_text = "succeeded";
+	else success_text = "failed";
+	console.log("\tTest ", success_text, "."); // success/failure output to console
+	return success; // return whether or not the test succeeded
 }
-
