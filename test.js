17e5444e5156edae329ae7d03bebc0382bfe0fe4
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-01-24 13:27:21 -0500

    test fiddletune.js

--- test.js
+++ test.js
@@ -14,10 +14,23 @@ var test_function = function(function_name, fun, inputs, expected_result) {
 	var result = fun(inputs); // compute the result on the given input
 	console.log("---Testing function",function_name,"on input", inputs,"---"); // intro output to console
 	console.log("\tResult:", result, "-- Expected result:", expected_result); // computation output to console
-	var success = (result === expected_result); // see if the result matches what's expected
+	var success = (result == expected_result); // see if the result matches what's expected
 	if(success)
 		success_text = "succeeded";
 	else success_text = "failed";
 	console.log("\tTest", success_text, "."); // success/failure output to console
 	return success; // return whether or not the test succeeded
 }
+
+// tests the fiddletune.js code
+var test_fiddletune = function() {
+	console.log("---Testing file fiddletune.js---");
+	var test1 = test_function("rhythmic_sequence", rhythmic_sequence, ([.25,.25,.5]),[[0,.25],[.25,.5],[.5,1]]);
+	var overall_result = test1;
+	if(overall_result)
+		success_text = "succeeded";
+	else success_text = "failed";
+	console.log("Code in fiddletune.js", success_text, ".");
+}
+
+test_fiddletune();
